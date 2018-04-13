1cba8e1324d5335bec0dfaffbc60a0eba4218d62
Robert Kerr <robrkerr@gmail.com>
2018-04-13 14:59:48 +1000

    Moved graphql queries into their own file. Renamed main file to seerpy.py and fixed a few issues with fetching channels without names and with a partial chunk at the end.

--- seerpy/__init__.py
+++ seerpy/__init__.py
@@ -1,1 +1,1 @@
-from .api import *
+from .seerpy import *
--- seerpy/auth.py
+++ seerpy/auth.py
@@ -2,8 +2,7 @@
 import requests
 import getpass
 
-
-class seerAuth:
+class SeerAuth:
 
     def __init__(self, apiUrl):
         self.apiUrl = apiUrl
@@ -16,7 +15,7 @@ class seerAuth:
                 print('Login Successful')
                 break
             elif response==401:
-                print('\nhttp error, please check your url: \n')
+                print('\HTTP error, please check your url: \n')
                 print(response)
                 self.cookie = None
                 break
@@ -25,23 +24,21 @@ class seerAuth:
                 print(response)
                 self.cookie = None
             else:
-                print('Login failed. please go to www.seermedical.com to reset your password')
+                print('Login failed. please go to app.seermedical.com to reset your password')
                 raise InterruptedError('Authentication Failed')
                 self.cookie = None
-    
 
     def login(self):
         apiUrl = self.apiUrl + '/api/auth/login'
         body = {'email': self.email, 'password': self.password}
         r = requests.post(url=apiUrl, data=body)
         self.cookie = {'seer.sid' : r.cookies['seer.sid']}
-    
+
     def verifyLogin(self):
         apiUrl = self.apiUrl + '/api/auth/verify'
         r = requests.get(url=apiUrl, cookies=self.cookie)
         return r.status_code
-    
+
     def loginDetails(self):
         self.email = input('Email Adddress: ')
         self.password = getpass.getpass('Password: ')
-

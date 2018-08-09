aca79db41d123e501b8e14e7dd6dc9509e067002
Robert Kerr <robrkerr@gmail.com>
2018-08-09 13:36:24 +1000

    Got download working with new studies... need to check this is right.

--- Examples/ContestDataDownloader.py
+++ Examples/ContestDataDownloader.py
@@ -1,5 +1,5 @@
 import numpy as np
-#import pandas as pd
+import pandas as pd
 #from pandas.io.json import json_normalize
 from datetime import datetime, timedelta, timezone
 # import h5py
@@ -24,7 +24,8 @@ import os
 ## studies to download
 ## pick from ['Pat1Test', 'Pat1Train', 'Pat2Test', 'Pat2Train', 'Pat3Test', 'Pat3Train']
 #studies = ['Pat1Test', 'Pat1Train', 'Pat2Test', 'Pat2Train', 'Pat3Test', 'Pat3Train']
-studies = ['Pat1Test']
+# studies = ['Pat1Test']
+studies = ['Pat1Test New']
 ## include a path to save downloaded data segments to file
 # path = 'D:/KAGGLE/data/ecosystem/test_download/' # replace with preferred path
 path = './' # replace with preferred path
@@ -34,10 +35,21 @@ path = './' # replace with preferred path
 # fileType = '.hdf5'
 fileType = '.mat'
 
+blockSize = 10 * 60 * 1000
+
+def getFilename(directory, study, fileType, blockStart, timezone, baseTime):
+    startTime = datetime.fromtimestamp(blockStart/1000, tz=timezone.utc)
+    hour = (startTime - baseTime).total_seconds()/3600
+    minute = startTime.minute
+    if minute >= 30:
+        preictal = 1
+    else:
+        preictal = 0
+    return directory + '/' + study + '_' + str(int(hour)) + '_' + str(preictal) + fileType
+
 if __name__ == '__main__':
 
-    GMToffset = 11 # Melb time
-    baseTime = datetime(2010,1,1,0,0, tzinfo=timezone.utc) + timedelta(hours=-GMToffset)
+    baseTime = datetime(2010,1,1,0,0, tzinfo=timezone.utc)
     try:
         dtMin = (baseTime + timedelta(hours=segmentMin)).timestamp()*1000
         dtMax = (baseTime + timedelta(hours=segmentMax+1)).timestamp()*1000
@@ -62,10 +74,8 @@ if __name__ == '__main__':
 
         allData = None
         allData = client.createMetaData(study)
+        sampleRate = allData['channelGroups.sampleRate'].iloc[0]
         
-        #return values in uV
-        allData['channelGroups.exponent'] = 0
-
         if dtMin is not None and dtMax is not None:
             allData = allData[(allData.loc[:,'segments.startTime']>=dtMin) & (allData.loc[:,'segments.startTime']<=dtMax)]
         
@@ -74,42 +84,53 @@ if __name__ == '__main__':
         print('  Downloading %d file(s)...' % numFiles)
         counter = 1
 
-        for chunk in allData['segments.startTime'].unique():
+        startTime = allData['segments.startTime'].unique()[0]
 
-            startTime = datetime.fromtimestamp(chunk/1000, tz=timezone.utc)
-            hour = (startTime - baseTime).total_seconds()/3600
-            minute = startTime.minute
-            if minute >= 30:
-                preictal = 1
-            else:
-                preictal = 0
+        while startTime <= allData['segments.startTime'].unique()[-1]:
 
-            #filename = study + '_' + str(int(hour)) + '_' + str(preictal)
-            filename = directory + '/' + study + '_' + str(int(hour)) + '_' + str(preictal) + fileType
+            blockStart = divmod(startTime, 1800)[0] * 1800
+            segments = allData[
+                (allData['segments.startTime'] >= blockStart) & (allData['segments.startTime'] < blockStart + blockSize)
+            ]['segments.startTime'].unique()
+
+            if len(segments) == 0:
+                break
+
+            filename = getFilename(directory, study, fileType, blockStart, timezone, baseTime)
             if os.path.exists(filename):
-                counter += 1
-                continue
-
-            b = ('   -> %s (%d/%d)' % (filename, counter, numFiles) + ''*200)
-            sys.stdout.write('\r'+b)
-            sys.stdout.flush()
-
-            ## Using threads>1 may speed up your downloads, but may also cause issues
-            ## on Windows systems. Use Carefully.
-            data = client.getLinks(allData[allData['segments.startTime']==chunk].copy(), threads=5)
-
-            if fileType == '.csv':
-                # csv format
-                data.to_csv(filename, index=False, float_format='%.3f')
-            elif fileType == '.hdf5':
-                # hdf5 format
-                data.to_hdf(filename, key='data', format='table')
-            elif fileType == '.mat':
-                # matlab files
-                savemat(filename, { 'data': np.asarray(data.iloc[:,-16:], dtype=np.float32) }, appendmat=False, do_compression=False)
+                counter += len(segments)
             else:
-                raise('Unknown file type for download')
-            counter += 1
+
+                data = pd.DataFrame(np.arange(blockStart, blockStart + blockSize, 1000 / sampleRate, dtype=np.int64), columns=['time'])
+
+                for segmentStartTime in segments:
+
+                    b = ('   -> %s (%d/%d)' % (filename, counter, numFiles) + ''*200)
+                    sys.stdout.write('\r'+b)
+                    sys.stdout.flush()
+
+                    ## Using threads>1 may speed up your downloads, but may also cause issues
+                    ## on Windows systems. Use Carefully.
+                    segmentData = client.getLinks(allData[allData['segments.startTime']==segmentStartTime].copy(), threads=5)
+                    data = data.merge(segmentData)
+                    counter += 1
+
+                if fileType == '.csv':
+                    # csv format
+                    data.to_csv(filename, index=False, float_format='%.3f')
+                elif fileType == '.hdf5':
+                    # hdf5 format
+                    data.to_hdf(filename, key='data', format='table')
+                elif fileType == '.mat':
+                    # matlab files
+                    savemat(filename, { 'data': np.asarray(data.iloc[:,-16:], dtype=np.float32) }, appendmat=False, do_compression=False)
+                else:
+                    raise('Unknown file type for download')
+
+            remainingTimes = allData[(allData['segments.startTime'] > blockStart + blockSize)]['segments.startTime'].unique()
+            if len(remainingTimes) == 0:
+                break
+            startTime = remainingTimes[0]
 
         b = ('  Finished downloading study.' + ''*200)
         sys.stdout.write('\r'+b)

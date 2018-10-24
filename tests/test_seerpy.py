a0b7f90682f74985a6c6f2f3b5f3e7dbafa4c50a
Brendan Doyle <brendan@seermedical.com>
2018-10-24 14:26:48 +1100

    added tests for getAllMetaData

--- tests/test_seerpy.py
+++ tests/test_seerpy.py
@@ -14,6 +14,8 @@ from seerpy.seerpy import SeerConnect
 # pylint complains that the methods could be a function. this disables that warning.
 # pylint:disable=no-self-use
 
+test_data_dir = pathlib.Path(__file__).parent / "test_data"
+
 
 @mock.patch('seerpy.seerpy.SeerAuth', autospec=True)
 class TestSeerConnect:
@@ -39,24 +41,21 @@ class TestSeerConnect:
             SeerConnect()
 
 
+@mock.patch.object(SeerConnect, "getAllMetaData", autospec=True)
 @mock.patch.object(SeerConnect, "__init__", autospec=True, return_value=None)
 class TestCreateMetaData:
 
     # as we don't rely on anything in __init() I have mocked it for simplicity
 
-    @mock.patch.object(SeerConnect, "getAllMetaData", autospec=True)
-    def test_single_study(self, get_all_metadata,
-                          seer_connect_init):  # pylint:disable=unused-argument
+    def test_single_study(self, seer_connect_init,  # pylint:disable=unused-argument
+                          get_all_metadata):
 
         # setup
-        parent_dir = pathlib.Path(__file__).parent
-
-        with open(parent_dir / "test_data/study1_metadata.json", "r") as f:
+        with open(test_data_dir / "study1_metadata.json", "r") as f:
             test_input = json.load(f)
-        studies = {'studies': [test_input['study']]}
-        get_all_metadata.return_value = studies
+        get_all_metadata.return_value = {'studies': [test_input['study']]}
 
-        test_result = pd.read_csv(parent_dir / "test_data/study1_metadata.csv", index_col=0)
+        test_result = pd.read_csv(test_data_dir / "study1_metadata.csv", index_col=0)
 
         # run test
         result = SeerConnect().createMetaData()
@@ -64,37 +63,98 @@ class TestCreateMetaData:
         # check result
         assert result.equals(test_result)
 
-    @mock.patch.object(SeerConnect, "getAllMetaData", autospec=True)
-    def test_four_studies(self, get_all_metadata,
-                          seer_connect_init):  # pylint:disable=unused-argument
+    def test_four_studies(self, seer_connect_init,  # pylint:disable=unused-argument
+                          get_all_metadata):
 
         # setup
-        parent_dir = pathlib.Path(__file__).parent
+        studies = []
+        for i in range(1, 5):
+            filename = "study" + str(i) + "_metadata.json"
+            with open(test_data_dir / filename, "r") as f:
+                studies.append(json.load(f)['study'])
 
-        study_list = list()
+        get_all_metadata.return_value = {'studies': studies}
 
-        with open(parent_dir / "test_data/study1_metadata.json", "r") as f:
-            study = json.load(f)
-        study_list.append(study['study'])
-
-        with open(parent_dir / "test_data/study2_metadata.json", "r") as f:
-            study = json.load(f)
-        study_list.append(study['study'])
-
-        with open(parent_dir / "test_data/study3_metadata.json", "r") as f:
-            study = json.load(f)
-        study_list.append(study['study'])
-
-        with open(parent_dir / "test_data/study4_metadata.json", "r") as f:
-            study = json.load(f)
-        study_list.append(study['study'])
-
-        get_all_metadata.return_value = {'studies': study_list}
-
-        test_result = pd.read_csv(parent_dir / "test_data/studies1-4_metadata.csv", index_col=0)
+        test_result = pd.read_csv(test_data_dir / "studies1-4_metadata.csv", index_col=0)
 
         # run test
         result = SeerConnect().createMetaData()
 
         # check result
         assert result.equals(test_result)
+
+
+@mock.patch('seerpy.seerpy.GQLClient', autospec=True)
+@mock.patch('seerpy.seerpy.SeerAuth', autospec=True)
+class TestGetAllMetaData:
+
+    def test_no_study_param(self, seer_auth, gql_client):
+
+        # setup
+        seer_auth.return_value.cookie = {'seer.sid': "cookie"}
+
+        side_effects = []
+
+        # this is the call in getStudies()
+        with open(test_data_dir / "studies.json", "r") as f:
+            side_effects.append({'studies': json.load(f)})
+
+        # these are the calls from the loop in getAllMetaData()
+        expected_results = []
+        for i in range(1, 5):
+            filename = "study" + str(i) + "_metadata.json"
+            with open(test_data_dir / filename, "r") as f:
+                study = json.load(f)
+                side_effects.append(study)
+                expected_results.append(study['study'])
+
+        gql_client.return_value.execute.side_effect = side_effects
+
+        # run test
+        result = SeerConnect().getAllMetaData()
+
+        # check result
+        assert result == {'studies' : expected_results}
+
+    def test_existing_study_param(self, seer_auth, gql_client):
+
+        # setup
+        seer_auth.return_value.cookie = {'seer.sid': "cookie"}
+
+        side_effects = []
+
+        # this is the call in getStudies()
+        with open(test_data_dir / "studies.json", "r") as f:
+            side_effects.append({'studies': json.load(f)})
+
+        # these are the calls from the loop in getAllMetaData()
+        expected_results = []
+        with open(test_data_dir / "study1_metadata.json", "r") as f:
+            study = json.load(f)
+            side_effects.append(study)
+            expected_results = [study['study']]
+
+        gql_client.return_value.execute.side_effect = side_effects
+
+        # run test
+        result = SeerConnect().getAllMetaData("Study 1")
+
+        # check result
+        assert result == {'studies' : expected_results}
+
+    def test_nonexistent_study_param(self, seer_auth, gql_client):
+
+        # setup
+        seer_auth.return_value.cookie = {'seer.sid': "cookie"}
+
+        # this is the call in getStudies()
+        with open(test_data_dir / "studies.json", "r") as f:
+            gql_client.return_value.execute.return_value = {'studies': json.load(f)}
+
+        # run test
+        result = SeerConnect().getAllMetaData("Study 12")
+
+        # check result
+        assert result == {'studies' : []}
+        # the only call will be in getStudies()
+        assert gql_client.return_value.execute.call_count == 1

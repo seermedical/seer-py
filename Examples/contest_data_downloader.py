9f48773e95063d396d50f5f7b13b31ee4d24bdf1
Brendan Doyle <brendan@seermedical.com>
2019-02-06 15:15:25 +1100

    renamed Examples/ContestDataDownloader.py to contest_data_downloader.py

--- Examples/contest_data_downloader.py
+++ Examples/contest_data_downloader.py
@@ -13,37 +13,38 @@ import seerpy
 ######################
 # Change this section for different studies / segment filters
 
-## Filter the amount of data returned by date - comment out segment_min and segment_max to download
+## Filter the amount of data returned by date - comment out SEGMENT_MIN and SEGMENT_MAX to download
 ## all study data.
-## If you experience connection breaks you may need to specify specific values for segment_min and
-## segment_max to download a specific range of data segments.
+## If you experience connection breaks you may need to specify specific values for SEGMENT_MIN and
+## SEGMENT_MAX to download a specific range of data segments.
 ## For 'Pat1Test', 'Pat1Train', 'Pat2Test', 'Pat2Train', 'Pat3Test', 'Pat3Train' the values for
-## segment_min and segment_max should be chosen within the ranges of [1,216], [1,1728], [1,1002],
+## SEGMENT_MIN and SEGMENT_MAX should be chosen within the ranges of [1,216], [1,1728], [1,1002],
 ## [1,2370], [1,690], [1,2396], respectively, and the total number of data segments is 216, 826,
 ## 1002, 2058, 690, 2163 respectively.
 ## Note that for training data the segment index preserves temporal order in the data but is not
 ## necessarily continuous, while for testing data the segment index is randomised and so does not
 ## preserve temporal order in the data.
-segment_min = 1
-segment_max = 5
+SEGMENT_MIN = 1
+SEGMENT_MAX = 5
+
+GMT_OFFSET = 11 # Melb time
 
 ## studies to download
 ## pick from ['Pat1Test', 'Pat1Train', 'Pat2Test', 'Pat2Train', 'Pat3Test', 'Pat3Train']
 #studies = ['Pat1Test', 'Pat1Train', 'Pat2Test', 'Pat2Train', 'Pat3Test', 'Pat3Train']
-studies = ['Pat1Test']
+STUDIES = ['Pat1Test']
 
 ## include a path to save downloaded data segments to file
-# path = 'D:/KAGGLE/data/ecosystem/test_download/' # replace with preferred path
-path = './test_download/' # replace with preferred path
+# OUTPUT_PATH = 'D:/KAGGLE/data/ecosystem/test_download/' # replace with preferred path
+OUTPUT_PATH = './test_download/' # replace with preferred path
 
 
-if __name__ == '__main__':
-
-    GMT_OFFSET = 11 # Melb time
+def main():  # pylint:disable=too-many-locals
+    """Entry point if called as an executable"""
     base_date_time = datetime(2010, 1, 1, 0, 0, tzinfo=timezone.utc) + timedelta(hours=-GMT_OFFSET)
     try:
-        min_date_time = (base_date_time + timedelta(hours=segment_min)).timestamp() * 1000
-        max_date_time = (base_date_time + timedelta(hours=segment_max+1)).timestamp() * 1000
+        min_date_time = (base_date_time + timedelta(hours=SEGMENT_MIN)).timestamp() * 1000
+        max_date_time = (base_date_time + timedelta(hours=SEGMENT_MAX+1)).timestamp() * 1000
     except NameError:
         print('No segment filter provided (downloading all data)')
         min_date_time = None
@@ -51,15 +52,15 @@ if __name__ == '__main__':
 
     client = seerpy.SeerConnect()
 
-    for study in studies:
-        directory = path + study
+    for study in STUDIES:
+        directory = OUTPUT_PATH + study
 
         # we could use exist_ok=True in python >= 3.2, but this should also work in python 2.7
         if not os.path.exists(directory):
             try:
                 os.makedirs(directory)
-            except OSError as e: # Guard against race condition
-                if e.errno != errno.EEXIST:
+            except OSError as ex: # Guard against race condition
+                if ex.errno != errno.EEXIST:
                     raise
 
         print('\nStudy: ', study)
@@ -93,7 +94,7 @@ if __name__ == '__main__':
 
             filename = directory + '/' + study + '_' + str(int(hour)) + '_' + str(preictal)
 
-            # write out a refreshing profress line
+            # write out a refreshing progress line
             progress = ('   -> %s (%d/%d)' % (filename, counter, num_files) + ' ' * 20)
             sys.stdout.write('\r' + progress)
             sys.stdout.flush()
@@ -118,8 +119,12 @@ if __name__ == '__main__':
 
             counter += 1
 
-        # write out a refreshing profress line
+        # write out a refreshing progress line
         progress = ('  Finished downloading study.' + ' ' * 20)
         sys.stdout.write('\r' + progress)
         sys.stdout.flush()
         print('')
+
+
+if __name__ == '__main__':
+    main()

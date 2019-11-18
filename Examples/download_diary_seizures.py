5981306c936bd2db773ca875b8c47a5c18502410
Pip Karoly <pkaroly@student.unimelb.edu.au>
2019-11-18 17:28:45 +1100

    add example casex

---
+++ Examples/download_diary_seizures.py
@@ -0,0 +1,17 @@
+import seerpy
+
+seer_client = seerpy.SeerConnect()
+
+patients = seer_client.get_patients_dataframe() #party_id can be blank to return all results
+for n in range(len(patients)):
+
+    patient_id = patients.loc[n]['id']
+    diary_df = seer_client.get_diary_labels_dataframe(patient_id)
+    if 'labelType' in diary_df.columns and not diary_df.loc[diary_df['labelType'] == 'seizure'].empty:
+        nEvents = diary_df.loc[diary_df['labelType'] == 'seizure'].iloc[0]['numberOfLabels']
+        if nEvents >= 10:
+            events = diary_df.loc[diary_df['labelType'] == 'seizure']
+            mdict = {'data': events}
+            savename = 'Patient '  + str(n)
+            print('saving ' + savename + '...')
+            events.to_excel(savename + '.xlsx')

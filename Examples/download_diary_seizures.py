4a01e11939366bee7ea31ab81a750ba04228c94b
Pip Karoly <pkaroly@student.unimelb.edu.au>
2020-01-16 12:42:56 +1100

    style changes in example

--- Examples/download_diary_seizures.py
+++ Examples/download_diary_seizures.py
@@ -6,23 +6,26 @@ N_EVENTS = 10
 
 seer_client = seerpy.SeerConnect()
 
-patients = seer_client.get_patients_dataframe() #party_id can be blank to return all results
+# party_id can be blank to return all results
+patients = seer_client.get_patients_dataframe()
+patient_ids = patients['id']
 
-for n in range(len(patients)):
+for patient_id in patient_ids:
 
-    patient_id = patients.loc[n]['id']
-    
     diary_df = seer_client.get_diary_labels_dataframe(patient_id)
-    
+
 #    Limit label groups to people with an epilepsy diary (where the default labelType is seizure)
-    if 'labelType' in diary_df.columns and not diary_df.loc[diary_df['labelType'] == 'seizure'].empty:
-        
-        nEvents = diary_df.loc[diary_df['labelType'] == 'seizure'].iloc[0]['numberOfLabels']
-        
-        if nEvents >= N_EVENTS:
-#            only save the events of interest
+    if diary_df is not None and \
+        'labelType' in diary_df.columns \
+            and not diary_df.loc[diary_df['labelType'] == 'seizure'].empty:
+
+        n_events = diary_df.loc[diary_df['labelType']
+                                == 'seizure'].iloc[0]['numberOfLabels']
+
+        if n_events >= N_EVENTS:
+            #            only save the events of interest
             events = diary_df.loc[diary_df['labelType'] == 'seizure']
             mdict = {'data': events}
-            savename = 'Patient '  + str(n)
+            savename = 'P_' + patient_id.replace('-', '')
             print('saving ' + savename + '...')
             events.to_excel(savename + '.xlsx')

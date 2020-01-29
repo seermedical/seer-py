5cf81007380ad0dcd21d4fee4e9bd2ba4bfdd470
Pip Karoly <pkaroly@student.unimelb.edu.au>
2020-01-29 14:08:58 +1100

    update examples

--- Examples/fitbit_sleep.py
+++ Examples/fitbit_sleep.py
@@ -1,4 +1,7 @@
 import seerpy
+from datetime import datetime
+from datetime import timedelta
+from dateutil import tz
 
 seer_client = seerpy.SeerConnect()
 
@@ -7,13 +10,22 @@ fitbit_sleep_name = 'Fitbit Sleep Labels'
 
 patients = seer_client.get_patients_dataframe()
 
+# Get the previous week worth of data
+today = datetime.utcnow().date()
+end_time = datetime(today.year, today.month, today.day, tzinfo=tz.tzutc())
+start_time = end_time - timedelta(days=7)
+
+# time stamps are in ms
+from_time = start_time.timestamp() * 1000
+to_time = end_time.timestamp() * 1000
+
 for n in range(len(patients)):
 
     patient_id = patients.loc[n]['id']
     patient_email = patients.loc[n]['user.email']
     
-#    test on i.e. Pip's data
-    if patient_email == 'pip@seermedical.com':
+#    get data for i.e. patient email
+    if patient_email == 'EMAIL':
         
 #        Get available label groups
         diary_data_groups = seer_client.get_diary_data_groups_dataframe(patient_id)
@@ -21,7 +33,7 @@ for n in range(len(patients)):
         for group in range(len(diary_data_groups)):
             group_name = diary_data_groups.loc[group]['name']
     
-#            get the label group for fitbit sleep labels
+    #            get the label group for fitbit sleep labels
             if  group_name == fitbit_sleep_name:
                 group_id = diary_data_groups.loc[group]['id']
-                sleep_labels = seer_client.get_diary_data_labels_dataframe(patient_id, group_id)
+                sleep_labels = seer_client.get_diary_data_labels_dataframe(patient_id, group_id, from_time, to_time)

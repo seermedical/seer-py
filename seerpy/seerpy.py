b81104caf94db67e28c31e3b85bae1aa8fb8d8fc
dominique <dominique@seermedical.com>
2020-03-19 09:46:42 +1100

    Remove empty line before return statement

--- seerpy/seerpy.py
+++ seerpy/seerpy.py
@@ -587,7 +587,6 @@ class SeerConnect:  # pylint: disable=too-many-public-methods
 
         query_string = graphql.get_diary_medication_compliance_query_string(patient_id, from_time, to_time)
         response = self.execute_query(query_string)
-
         return response
 
     def get_diary_medication_compliance_dataframe(self, patient_id, from_time=0, to_time=0):

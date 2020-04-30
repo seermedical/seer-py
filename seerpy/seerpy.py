d2ff7a8d40d484e6b88febde887e32b628030de6
dominique <dominique@seermedical.com>
2020-05-01 09:43:55 +1000

    Return negative of output type instead of None

--- seerpy/seerpy.py
+++ seerpy/seerpy.py
@@ -500,6 +500,11 @@ class SeerConnect:  # pylint: disable=too-many-public-methods
             return orgs
         return pd.DataFrame(orgs)
 
+    def get_patient(self, patient_id):
+        query_string = graphql.get_patient_query_string(patient_id)
+        response = self.execute_query(query_string)['patient']
+        return response
+
     def get_patients(self, party_id=None):
         query_string = graphql.get_patients_query_string()
         response = self.execute_query(query_string, party_id)['patients']
@@ -527,6 +532,11 @@ class SeerConnect:  # pylint: disable=too-many-public-methods
                 document['name'] = study['name']
                 documents.append(document)
         return pd.DataFrame(documents)
+
+    def get_diary_created_at(self, patient_id):
+        query_string = graphql.get_diary_created_at_query_string(patient_id)
+        response = self.execute_query(query_string)['patient']['diary']['createdAt']
+        return response
 
     def get_diary_labels(self, patient_id, label_type='all', offset=0, limit=100, from_time=0, to_time=9e12, from_duration=0, to_duration=9e12):
         """
@@ -617,6 +627,7 @@ class SeerConnect:  # pylint: disable=too-many-public-methods
         label_groups = label_groups.merge(tags, how='left', on='labels.id', suffixes=('', '_y'))
         label_groups = label_groups.rename({'id':'labelGroups.id'})
         label_groups['id'] = patient_id
+        label_groups['createdAt'] = label_results['createdAt']
         return label_groups
 
     def get_diary_medication_alerts(self, patient_id, from_time=0, to_time=9e12):

1f8118a5bf19b0439df318872093a8cfc6653b06
dkeden <56851238+dkeden@users.noreply.github.com>
2020-06-15 16:47:28 +1000

    Remove check for no label_groups

--- seerpy/seerpy.py
+++ seerpy/seerpy.py
@@ -1163,8 +1163,6 @@ class SeerConnect:  # pylint: disable=too-many-public-methods
             return pd.DataFrame()
 
         label_groups = json_normalize(label_results['labelGroups']).sort_index(axis=1)
-        if label_groups.empty:
-            return pd.DataFrame(label_results)
         labels = self.pandas_flatten(label_groups, '', 'labels')
         tags = self.pandas_flatten(labels, 'labels.', 'tags')
 

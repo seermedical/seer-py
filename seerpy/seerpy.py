1a8c642822c4904ada9e60650954be0c7c90cbf6
Dominique Eden <dominique@seermedical.com>
2020-08-27 14:39:25 +1000

    Add query variable to get_patients() function

--- seerpy/seerpy.py
+++ seerpy/seerpy.py
@@ -983,7 +983,7 @@ class SeerConnect:  # pylint: disable=too-many-public-methods
         """
         query_string = graphql.get_patients_query_string()
         response = self.execute_query(query_string, party_id)
-        return response['patients']
+        return response['getPatientList']['patients']
 
     def get_patients_dataframe(self, party_id=None):
         """
